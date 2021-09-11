d94a053f0c559fa469525cf9d2397cc1210c9475
Y. Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2021-09-11 11:16:03 +0300

    Fix an issue about that stdin is not typed when only stderr is a tty

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # dutty
-simple teletype test dummy written in Rust
+simple teletype test dummy written in Rust.
+
+## Usage
+```sh
+$ dutty 
+Standard output is a TERMINAL
+Standard input is a TERMINAL
+Standard error is a TERMINAL
+```
+Here, `dutty` recognised that its stdin, stdout and stderr streams were all from the terminal; so it told us that these streams were all terminals.
+
+Let's try to redirect its standard IO to teletypes:
+1. Stdin
+  ```sh
+  $ dutty < somefile
+  Standard output is a TERMINAL
+  Standard input is a TELETYPE
+  Standard error is a TERMINAL
+  ```
+  Here, we redirected `dutty`'s standart input to a file (which is a teletype), so it told us that stdin was a teletype.
+
+  This also works with pipes:
+  ```sh
+  $ head /dev/random | dutty
+  Standard output is a TERMINAL
+  Standard input is a TELETYPE
+  Standard error is a TERMINAL
+  ```
+  You may also notice that `dutty` colorizes the words `TERMINAL` and `TELETYPE` green and red, respectively. It does this even when its stdout or stderr is redirected, cause it uses the *stubborn I/O* (trying to write exactly to the console, regardless of the state of its stdio).
+
+2. Stdout
+  `dutty` has a similar behaviour for stdout.
+
+  ```sh
+  $ dutty | cat
+  Standard output is a TELETYPE
+  Standard input is a TERMINAL
+  Standard error is a TERMINAL
+  ```
+3. Stderr
+  `dutty` has the same behavior when its standard error is redirected.
+  ```sh
+  $ dutty 2>| cat
+  Standard output is a TERMINAL
+  Standard input is a TERMINAL
+  Standard error is a TELETYPE
+  ```
+
+  ```sh
+  $ dutty |& cat
+  Standard output is a TELETYPE
+  Standard input is a TERMINAL
+  Standard error is a TELETYPE
+  ```
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use atty::Stream;
 use clap::{App, Shell};
-use crossterm::style::{self, Stylize, Color};
+use crossterm::style::{self, Color, Stylize};
 
 fn main() {
     let _cli = App::new("dutty") // Configure the CLI
         .version("1.0.0") // Version 1.0.0
         .author("Y. Cemal Öztürk <ozturkyigitcemal@gmail.com>") // Author information
         .about("inspects the states of terminal and teletype stdio streams") // What dutty is
-        .usage("dutty [-h | --help] [-V | --version]");  // and how to use it
-    let (color_terminal, color_teletype) = ("TERMINAL".green(), "TELETYPE".red());  
+        .usage("dutty [-h | --help] [-V | --version]"); // and how to use it
+    let (color_terminal, color_teletype) = ("TERMINAL".green(), "TELETYPE".red());
     let (is_stdout, is_stdin, is_stderr) = (
         atty::is(Stream::Stdout),
         atty::is(Stream::Stdin),
-        atty::is(Stream::Stderr)
+        atty::is(Stream::Stderr),
     ); // Check wheter the streams are piped
     if is_stdout {
         println!("Standard output is a {}", color_terminal); //stdout is terminal
@@ -21,9 +21,17 @@ fn main() {
     }
 
     if is_stdin {
-        eprintln!("Standard input is a {}", color_terminal); // stdin is terminal
+        if !is_stderr {
+            println!("Standard input is a {}", color_terminal); // stdin is terminal
+        } else {
+            eprintln!("Standard input is a {}", color_terminal);
+        }
     } else {
-        eprintln!("Standard input is a {}", color_teletype); // stdin is tty
+        if is_stderr {
+            eprintln!("Standard input is a {}", color_teletype); // stdin is tty
+        } else {
+            println!("Standard input is a {}", color_teletype);
+        }
     }
 
     if is_stderr {
