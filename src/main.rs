ab6b98f0413e898980c86d20a89beed1cea48e94
Y. Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2021-09-11 10:46:37 +0300

    Add colorisation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use atty::Stream;
 use clap::{App, Shell};
+use crossterm::style::{self, Stylize, Color};
 
 fn main() {
     let _cli = App::new("dutty") // Configure the CLI
@@ -7,28 +8,27 @@ fn main() {
         .author("Y. Cemal Öztürk <ozturkyigitcemal@gmail.com>") // Author information
         .about("inspects the states of terminal and teletype stdio streams") // What dutty is
         .usage("dutty [-h | --help] [-V | --version]");  // and how to use it
-        
-
+    let (color_terminal, color_teletype) = ("TERMINAL".green(), "TELETYPE".red());  
     let (is_stdout, is_stdin, is_stderr) = (
         atty::is(Stream::Stdout),
         atty::is(Stream::Stdin),
         atty::is(Stream::Stderr)
     ); // Check wheter the streams are piped
     if is_stdout {
-        println!("Standard output is a TERMINAL"); //stdout is terminal
+        println!("Standard output is a {}", color_terminal); //stdout is terminal
     } else {
-        eprintln!("Standard output is a TELETYPE"); //stdout is tty
+        eprintln!("Standard output is a {}", color_teletype); //stdout is tty
     }
 
     if is_stdin {
-        eprintln!("Standard input is a TERMINAL"); // stdin is terminal
+        eprintln!("Standard input is a {}", color_terminal); // stdin is terminal
     } else {
-        eprintln!("Standard input is a TELETYPE"); // stdin is tty
+        eprintln!("Standard input is a {}", color_teletype); // stdin is tty
     }
 
     if is_stderr {
-        eprintln!("Standard error is a TERMINAL"); // stderr is terminal
+        eprintln!("Standard error is a {}", color_terminal); // stderr is terminal
     } else {
-        println!("Standard error is a TELETYPE"); // stderr is tty
+        println!("Standard error is a {}", color_teletype); // stderr is tty
     }
 }
